48f0463b69a623efc7d35cc43874ec77b9c69dbb
Kawaii-programmer <roma.siyamenko.100@mail.ru>
2024-10-12 18:57:41 +0300

    Asset's commit
    Added unity project's files

---
+++ Assets/Renderers/OutlineFeature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace Assets.Renderers
+{
+    [Serializable]
+    internal class OutlineFeature : ScriptableRendererFeature
+    {
+        [SerializeField] private RTHandle _handle;
+        [SerializeField] private RenderTexture _text;
+        [SerializeField] private LayerMask _mask;
+        [SerializeField] private Settings _settings = new Settings();
+        [SerializeField] private Material _material;
+
+        [Serializable] 
+        public class Settings
+        {
+            public Material material;
+        }
+
+        private OutlinePass pass;
+        private RTHandle _rtHandle;
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (_text != null)
+            {
+                renderer.EnqueuePass(pass);
+            }
+        }
+
+        public override void Create()
+        {
+            if (_text != null)
+            {
+                _rtHandle = RTHandles.Alloc(_text);
+                pass = new OutlinePass(_rtHandle, _mask, _settings.material);
+            }
+        }
+    }
+}
